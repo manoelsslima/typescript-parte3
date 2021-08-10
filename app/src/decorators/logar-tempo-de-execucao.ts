799aeab2876496907c912880485886a30f547487
Manoel de Souza Silva Lima <manoel.lima@tjac.jus.br>
2021-08-10 03:46:10 -0500

    Ajustes nas configurações da aplicação

--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,9 +1,20 @@
 export function logarTempoDeExecucao() {
+    // descriptor dá acesso ao método original anotado
     return function(
         target: any,
-        property: string,
+        propertyKey: string,
         descriptor: PropertyDescriptor
     ) {
+        const metodoOriginal = descriptor.value;
+        descriptor.value = function(...args: Array<any>) {
+            const t1 = performance.now();
+            // apply - permite passar o contexto e um array de parâmetros
+            // this -> será a classe que chama o método original
+            const retorno = metodoOriginal.apply(this, args);
+            const t2 = performance.now();
+            console.log(`${propertyKey}, tempo de execução ${(t2-t1)/1000} segundos`);
+            return retorno;
+        };
         return descriptor;
     }
 }
