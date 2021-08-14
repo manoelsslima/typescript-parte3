0c52df11fa6130118695a1a1006ec76daf6e6bd6
Manoel de Souza Silva Lima <manoel.lima@tjac.jus.br>
2021-08-14 18:21:57 -0500

    Decorator @domInjector corrigido

--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -2,22 +2,27 @@ export function domInjector(seletor: string) {
     // propertyKey mostra a propriedade onde o decorator foi adicionado
     // pode ser métodos ou atributos
     // é aplicado na hora que o javascript tá criando a classe
+    // o decorator de método tem o parâmetro: descriptor
     return function(target: any, propertyKey: string){
         console.log(`Modificando prototype ${target.constructor.name} e adicionando
         getter para a propriedade ${propertyKey}`);
 
-        // cria um getter - sempre que acessar a propriedade, retorno o elemento do DOM
-        const getter = function() {
-            const elemento = document.querySelector(seletor);
-            console.log(`buscando elemento do DOM com seletor ${seletor} para injetar em ${propertyKey}`);
+        let elemento: HTMLElement;
+
+        // cria um getter na propriedade
+        const getter = function() { // quando acessar a propriedade, retorna o elemento do DOM
+            if (!elemento) { // cache decorator copy - evita buscar o elemento novamente
+                elemento = <HTMLElement>document.querySelector(seletor);
+                console.log(`buscando elemento do DOM com seletor ${seletor} para injetar em ${propertyKey}`);
+            }
             return elemento;
         }
 
-        // substitui a propriedade original da classe por um getter
+        // substitui, no prototype (definição da classe), a propriedade original por um getter
         Object.defineProperty(
             target,
             propertyKey,
-            { get: getter }
+            { get: getter } // cria uma propriedade get chamando o método getter
         )
     }
 }
